b812171c5ad5f25097b9c42b2db4fa3319f602ca
Hukla <129692708+huklaa@users.noreply.github.com>
2026-08-24 18:20:08 +0300

    test: make guide schema sidebar injectable

--- params.ts
+++ params.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 import { getFormattedSidebar } from "./utils.js";
 
-export function getGuideParams() {
+export function getGuideParams(sidebar = getFormattedSidebar()) {
   const findGuideParamsPrompt = `
     This is the path to the technical documentation to create actions from.
     To get the steps list, you need to pass the guideLink to the BuildOnBase getGuide tool.
     From the user prompt, find the most relevant guide from the sidebar of the docs website.
     The sidebar list can be found here:
-    ${getFormattedSidebar()}
+    ${sidebar}
     Find the path of the guide in the sidebar and pass it as guideLink by adding https://docs.base.org to the getStepsList tool.
 
     For example, if the user wants to create a sign and verify component, the guideLink should be https://docs.base.org/identity/smart-wallet/guides/signing-and-verifying-messages
